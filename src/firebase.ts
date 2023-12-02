7e20e09248f51bd102f99794d94662b5770e8831
vivekkum-ar <vivekkr9119@gmail.com>
2023-12-02 22:14:20 +0530

    Added firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIpPvFSdqEKapPRm9Q8TjEhUCaop39jVQ",
+  authDomain: "earthquake-detection-adxl.firebaseapp.com",
+  databaseURL: "https://earthquake-detection-adxl-default-rtdb.firebaseio.com",
+  projectId: "earthquake-detection-adxl",
+  storageBucket: "earthquake-detection-adxl.appspot.com",
+  messagingSenderId: "382436219205",
+  appId: "1:382436219205:web:f2758b741ee6a77a3e862c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
