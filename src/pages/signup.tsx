2f7c6225d50ef3b8ae2d9864017a1dd272b9e956
vivekkum-ar <vivekkr9119@gmail.com>
2023-12-03 15:25:40 +0530

    feat: added signup UI

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,93 @@
-import React from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-interface SignUpProps {
-  // Add your prop types here
-}
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { toast } from "sonner"
 
-const SignUp: React.FC<SignUpProps> = ({}) => {
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+})
+
+export function SignUpForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data.username);
+    toast("You submitted the following values:", {
+      description: (`username:${data.username} \n password:${data.password}`),
+    })
+
+
+  }
+
   return (
-    <div>SignUp</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 justify-center flex flex-col space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="●●●●●●●●" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
-export default SignUp
+export function SignUp() {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-screen-sm px-12 mx-auto">
+      <SignUpForm />
+    </div>
+  )
+}
